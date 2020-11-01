962264c9e055f3223687e4a5b16d72998d4202bb
sanjeev <sanjeev.develop@gmail.com>
2020-11-01 19:02:32 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as Highcharts from "highcharts/highstock";
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'highchart-salesApp';
+  Highcharts: typeof Highcharts = Highcharts;
+  chartOptions: Highcharts.Options = {
+    title: {
+      text: 'highchart-salesApp'
+    },
+
+  yAxis: {
+        title: {
+          text: "Sales"
+        },
+},
+
+tooltip: {
+  valueSuffix: '$'
+},
+series: [
+    {
+      name: 'Sales',
+      data: [
+        [1980, 14000],
+        [1985, 22000],
+        [1990, 104000],
+        [1995,  87000],
+        [2000, 43400],
+      ],
+    },
+  ]   
+  }
 }
